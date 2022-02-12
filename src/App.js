10b3066a6778f23c5c2a83c36c9e0a1928f1d4f6
Fagbemi Oluwaseyi <seyifagbemi211@gmail.com>
2022-02-13 00:47:48 +0100

    Completed Love calculator

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from "react";
 import LoveForm from "./components/LoveForm";
 import "./App.css";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className="app">
-      <img
-        className="app-img"
-        src="https://www.freeiconspng.com/uploads/love-png-5.png"
-        alt="love"
-      />
-      <h1>LOVE CALCULATOR BY OLUWASEYI</h1>
-      <div className="app-form">
-        <LoveForm />
+    <>
+      <Header />
+      <div className="app">
+        <img
+          className="app-img"
+          src="https://www.freeiconspng.com/uploads/love-png-5.png"
+          alt="love"
+        />
+        <h1>LOVE CALCULATOR BY OLUWASEYI</h1>
+        <div className="app-form">
+          <LoveForm />
+        </div>
       </div>
-    </div>
+      <footer>
+        <div>
+          <p> Made with Love by Oluwaseyi</p>
+          <p>&copy; 2022</p>
+        </div>
+      </footer>
+    </>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
     <header>
-      <img
-        className="app-img"
-        src="https://www.freeiconspng.com/uploads/love-png-5.png"
-        alt="love"
-      />
+      <div>
+        <h1>The Love Meter</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a
+              href="https://twitter.com/xoluwaseyi"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <i class="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/xoluwaseyi"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <i class="fa fa-github" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://seyiportfolio.netlify.app"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <i class="fa fa-user-circle" aria-hidden="true"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
-}
+};
 
-export default Header
+export default Header;
